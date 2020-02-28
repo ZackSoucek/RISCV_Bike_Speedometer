70f687b5682880d38f917ccd735b979b7b8ebf44
d-mooers <49253708+d-mooers@users.noreply.github.com>
2020-02-28 10:03:16 -0800

    Add files via upload

---
+++ riscv1.asm
@@ -0,0 +1,43 @@
+main: li a3 0x110C0000 # SSEG
+li a2 966853 # for mpg calculation
+li s1 999999
+li s5 0
+la t1 ISR
+csrw t1 mtvec
+li s2 1
+csrw s2 mie
+li a1 0
+#sw a1 0(a3)
+
+loop: addi a1 a1 1
+sw a1 0(a3)
+j loop
+
+ISR: csrw zero mie
+beqz s5 ISR_MAIN
+li s5 0
+li a1 0
+li s2 1
+csrw s2 mie
+j loop
+
+ISR_MAIN: 
+jal div
+sw a0 0(a3)
+li s5 1
+li s2 1
+csrw s2 mie
+ISR_LOOP: nop
+j ISR_LOOP
+
+# a0 = a2 / a1
+div:
+li a0 0
+mv s3 a2
+beqz a1 endDiv
+divLoop: blt s3 a1 endDiv
+sub s3 s3 a1
+addi a0 a0 1
+j divLoop
+endDiv: jalr ra
+
